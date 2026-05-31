0f61cfeb3af46429346ad0360f36942b4c60f5b9
mikey820 <mikeyditaranto@gmail.com>
2026-05-31 18:02:32 -0400

    Add missing typebanner stub functions for CI build

--- Cyanide/tweaks/private/typebanner.h
+++ Cyanide/tweaks/private/typebanner.h
@@ -5,12 +5,16 @@
 #import <Foundation/Foundation.h>
 @class RemoteCallSession;
 #define TYPEBANNER_RC_FIRST_EXCEPTION_TIMEOUT_MS 3000
+#define TYPEBANNER_RC_MOBILESMS_FIRST_EXCEPTION_TIMEOUT_MS 3000
 void typebanner_forget_remote_state(void);
 bool typebanner_has_remote_state(void);
+bool typebanner_prepare_in_springboard_session(void);
 bool typebanner_release_mobilesms_keepalive_in_springboard_session(void);
 bool typebanner_hide_in_springboard_session(void);
 bool typebanner_run_once_with_mobile_session_and_current_springboard(RemoteCallSession * _Nullable * _Nullable session, bool springboardReady);
 void typebanner_release_mobilesms_keepalive_in_springboard_remote_session(RemoteCallSession * _Nonnull session);
 void typebanner_hide_in_springboard_remote_session(RemoteCallSession * _Nonnull session);
+NSString * _Nullable typebanner_poll_in_imagent_remote_session(RemoteCallSession * _Nonnull session);
+bool typebanner_show_in_springboard_remote_session(RemoteCallSession * _Nonnull session, NSString * _Nonnull label);
 #endif
 #endif
--- Cyanide/tweaks/private/typebanner.m
+++ Cyanide/tweaks/private/typebanner.m
@@ -2,8 +2,11 @@
 #import "../../TaskRop/RemoteCall.h"
 void typebanner_forget_remote_state(void) {}
 bool typebanner_has_remote_state(void) { return false; }
+bool typebanner_prepare_in_springboard_session(void) { return false; }
 bool typebanner_release_mobilesms_keepalive_in_springboard_session(void) { return false; }
 bool typebanner_hide_in_springboard_session(void) { return false; }
 bool typebanner_run_once_with_mobile_session_and_current_springboard(RemoteCallSession **session, bool springboardReady) { (void)session; (void)springboardReady; return false; }
 void typebanner_release_mobilesms_keepalive_in_springboard_remote_session(RemoteCallSession *session) { (void)session; }
 void typebanner_hide_in_springboard_remote_session(RemoteCallSession *session) { (void)session; }
+NSString *typebanner_poll_in_imagent_remote_session(RemoteCallSession *session) { (void)session; return nil; }
+bool typebanner_show_in_springboard_remote_session(RemoteCallSession *session, NSString *label) { (void)session; (void)label; return false; }
